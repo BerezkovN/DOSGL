cfd45ed720e79b03e922a6c77bbc81cfee6acbd8
BerezkovN <nikita.berezkof@gmail.com>
2021-12-26 22:44:06 +0200

    Adding comments to BAT files

--- COMPILE.bat
+++ COMPILE.bat
@@ -3,6 +3,11 @@ echo //COMPILING DOSGL.LIB >> LOGS.TXT
 echo /////////////////// >> LOGS.TXT
 echo /                 / >> LOGS.TXT
 
+REM -ml - uses large memory model (IMPORTANT)
+REM -c 	- compile OBJ files
+REM -n 	- destination folder
+REM -I 	- include folder
+
 tcc -IINCLUDE -ml -c -nBIN SRC\dosgl.cpp SRC\dglm.cpp SRC\vga.cpp SRC\pipeline.cpp >> LOGS.TXT
 del BIN\DOSGL.LIB
 tlib /C BIN\DOSGL.LIB +BIN\DOSGL.OBJ+BIN\DGLM.OBJ+BIN\VGA.OBJ+BIN\PIPELINE.OBJ >> LOGS.TXT
--- TEST/TEST.bat
+++ TEST/TEST.bat
@@ -3,4 +3,7 @@ echo //COMPILING TEST.EXE >> LOGS.TXT
 echo ///////////////////  >> LOGS.TXT
 echo /                 /  >> LOGS.TXT
 
+REM -ml - uses large memory model (IMPORTANT)
+REM -I 	- include folder
+
 tcc -I..\INCLUDE -ml test.cpp ..\BIN\DOSGL.LIB >> LOGS.TXT
